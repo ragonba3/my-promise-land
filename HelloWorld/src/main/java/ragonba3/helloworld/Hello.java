5e5d861927fe77e5fe955bdd32ec479e38b1940c
ragonba3 <85961668+ragonba3@users.noreply.github.com>
2021-06-15 23:28:32 -0400

    Add files via upload

---
+++ HelloWorld/src/main/java/ragonba3/helloworld/Hello.java
@@ -0,0 +1,12 @@
+/**
+ *@author Rene Gonzalez
+ *email: ren_gon@rogers.com
+ *date: 2021-06-15
+ *purpose: My first Hello World program in an IDE
+ */
+
+//package my-promise-land.helloworld;
+
+public class Hello {
+    public static void main(String[] arg) {
+        System.out.println("Hello,Rene");}}
