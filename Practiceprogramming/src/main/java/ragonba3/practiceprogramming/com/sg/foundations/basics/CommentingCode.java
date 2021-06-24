c72d2d8dc6800053de2b14740dd79f9679795a12
Rene <ren_gon@rogers.com>
2021-06-23 20:57:49 -0400

    allmath not completed yet

---
+++ Practiceprogramming/src/main/java/ragonba3/practiceprogramming/com/sg/foundations/basics/CommentingCode.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ragonba3.practiceprogramming.com.sg.foundations.basics;
+
+/**
+ *
+ * @author ragonba3
+ */
+public class CommentingCode {
+    
+    public static void main(String[] args) {
+        
+    // Comments are written to explain code in an easily
+    // understandable way
+    // Basically for singles lines
+    // anything after // is considered a comment
+        System.out.println("Commets however ...");// do not execute!
+        
+        // Comments can be on their own line
+        System.out.println("..."); //or they can share like this
+        
+    // However if you put the  // BEFORE a line code
+    // System.out.println("Then it is considered a comment");
+    // System.out.println("and it wont execute!);
+    
+    /*
+            This is an example of multi-line comment, which is useful if
+            you want to commet out multiple lines of code quiclky.
+            Console.WriteLine("Java ignores evertyhing inside the comment markers.");
+    
+    */
+    
+    }
+    
+}
+ 
---
+++ Practiceprogramming/src/main/java/ragonba3/practiceprogramming/com/sg/foundations/basics/ProjectGutenberg.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ragonba3.practiceprogramming.com.sg.foundations.basics;
+
+/**
+ *
+ * @author ragonba3
+ */
+public class ProjectGutenberg {
+    
+    public static void main(String[] args) {
+        System.out.println("Did you know that in 1440 (or thereabouts),");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.println("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.println("We are like a modern Gutenberg,");
+        System.out.println("printing vast amounts to the waiting console with ease.");
+        
+    }
+    
+    
+}
