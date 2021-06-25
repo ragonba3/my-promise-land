e08fb87d7ab7d4db52fe19c5a9f83798a4ded084
Rene <ren_gon@rogers.com>
2021-06-24 22:02:25 -0400

    MenuOfChampions is completed

--- Practiceprogramming/src/main/java/com/sg/foundations/variables/MenuOfChampions.java
+++ Practiceprogramming/src/main/java/com/sg/foundations/variables/MenuOfChampions.java
@@ -16,12 +16,40 @@ public class MenuOfChampions {
 public static void main(String[] args) {
     
     
-    String Name, Greetings, Menu, Prices, 
+    double Number;
+    String name, greetings, menu, prices,BBQ, rice, manitoba, beverage, dessert, coffee, number, te,
+    String ;
     
-    name = "Our Comunity of St. Mary";
-    glorious = "WHERE FIND YOUR FOOD AND PEACE";
+    
+    number = "10.00";
+    name = "Our Community of St. Mary";
+    greetings = "WHERE YOU FIND YOUR FOOD AND PEACE";
+    menu = "Barbeque with Rice";
+    prices = "BBQ with Garlic Just  ";
+    BBQ = "$10";
+    te = "$2.00";
+    dessert = "$3.00";
+    beverage = "$1.50";
+    
+    System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    System.out.println("Welcom to  " + name );
+    System.out.println("" + greetings );
+    System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    
+    System.out.println("Our Special of the Day..." + prices +  BBQ );
+    
+    System.out.println("Take Out our Coffee for: "    + te  );
+    System.out.println("Or Take out :  " + menu +   BBQ);
     
     
+    
+    
+    
+    
+    
+    
+    
+     
     
     
     
