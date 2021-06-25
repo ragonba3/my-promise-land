862df304d53d1bc555d1f7d4a90bd2b0be11a841
Rene <ren_gon@rogers.com>
2021-06-24 23:15:59 -0400

    TheBestAdderEver does not sum, but multiply

---
+++ Practiceprogramming/src/main/java/com/sg/foundations/variables/BestAddeEver.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author xolot
+ */
+public class BestAddeEver {
+    
+    public static void main(String[] args) {
+        int sum = 0;
+        int operandA = 12;
+        int operandB = 3;
+        int operandC = sum;
+        
+        sum = operandA * operandB;
+        
+        System.out.println("The multiplication is : " + sum);
+        
+        
+        
+        
+        
+    }
+    
+}
