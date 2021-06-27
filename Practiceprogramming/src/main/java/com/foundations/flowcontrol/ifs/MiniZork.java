d0e0aa86ff499c560d67f188bc49e2cf6a8a4fea
Rene <ren_gon@rogers.com>
2021-06-27 02:10:32 -0400

    MiniZork nailedgit add --all

--- Practiceprogramming/src/main/java/com/foundations/flowcontrol/ifs/MiniZork.java
+++ Practiceprogramming/src/main/java/com/foundations/flowcontrol/ifs/MiniZork.java
@@ -17,8 +17,8 @@ public class MiniZork {
         
         Scanner userInput = new Scanner(System.in);
         
-        System.out.println("You are standing in a open field westof a white house,");
-        System.out.println("With a voarded front door,");
+        System.out.println("You are standing in a open field west of a white house,");
+        System.out.println("With a boarded front door.");
         System.out.println("There is a small mailbox here.");
         System.out.println("Go to the house, or open the mailbox");
         
@@ -29,16 +29,26 @@ public class MiniZork {
         System.out.println("You open the mailbox.");
         System.out.println("It's really dark in there.");
         System.out.println("Look inside or stick you hand in?  ");
-        action = userInput.nextLine();
+        action = userInput.nextLine();}
         
         if (action.equals("look inside")){
-            System.out.println("");
+            System.out.println("You peer inside the mailbox.");
+            System.out.println("It's rally very dark. So ... so very dark.");
+            System.out.println("Run away or keep looking? ");
+            action = userInput.nextLine();}
+            
+            if (action.equals("keep looking")){
+              System.out.println("Turns out, hanging out around dark places isn't a good idea.");
+                System.out.println("You've been eaten by a grue");}
+            else if (action.equals("run away")){
+                System.out.println("You run away screaming across the fields - looking very foolish");
+                
+                System.out.println("But you alive. Possibly a wise choice");}
+            
+            else if (action.equals ("stick your hand in")){}
+            else if (action.equals("go to the house")){}}
+              
         }
         
-            
-        
-    }
-        
-        
-    }
-}
+
+
