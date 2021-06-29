6b01a80b7b6de1233e9d7a1a42cd685edce94209
Rene <ren_gon@rogers.com>
2021-06-29 14:00:14 -0400

    flowcontrol

---
+++ Practiceprogramming/src/main/java/com/foundations/flowcontrol/ifs/PanDemicBus.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.foundations.flowcontrol.ifs;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ragonba3
+ */
+public class PanDemicBus {
+    
+    
+    public static void main(String[] args) {        
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("You are lining up - waiting for the bus");
+        System.out.println("Boarding the bus through the front door");
+        System.out.println("There is a fee to pay");
+        System.out.println("pay the fee or wait for the next bus");
+        
+        String action = userInput.nextLine();    
+    if (action.equals("pay the fee")){
+        System.out.println("board the bus.");
+        System.out.println("Do you have a mask?");
+        System.out.println("get your mask or you forgot it?");
+        action = userInput.nextLine();
+    
+    if (action.equals("have a mask")){
+        System.out.println("You know it's an unconfortable neccesity");
+        System.out.println("take it off or settled with?");
+        action = userInput.nextLine();
+    if (action.equals("I forgot it")){
+    
+        System.out.println("sorry cannot board the bus, no mask no boarding");
+        }else if(action.equals("settled with")){
+            System.out.println("Then have a productive day at Gulag, just like Vasily Kovalyov");
+    
+        action = userInput.nextLine();
+            
+        if (action.equals("take it off")){
+            System.out.println("sorry mask are a most put it back");
+        }else if (action.equals("wait for the next bus")){}
+        System.out.println("Next one coming in fifteen minutes");
+        System.out.println("Remenber call you mamager");
+        
+    
+        }}}}}
+    
+                    
+                    
+                    
+
+
+        
+        
+            
+        
+        
+    
+    
+                
+    
+
---
+++ Practiceprogramming/src/main/java/com/sg/foundations/flowcontrol/random/ALittleChaos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.flowcontrol.random;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ragonba3
+ */
+public class ALittleChaos {
+    
+    public static void main(String[] args) {
+        
+        Random randomizer = new Random();
+        
+        System.out.println("Random can make integers:" + randomizer.nextInt());
+        System.out.println("Or a double:" + randomizer.nextDouble());
+        System.out.println("Or even a boolean:" + randomizer.nextBoolean());
+        
+        int num = randomizer.nextInt(64);
+        
+        System.out.println("you can store a randomizer result:" + num);
+        System.out.println("And use it over and over again: " + num + "," + num);
+        
+        System.out.println("Or Just keep generating new values");
+        System.out.println("Here is bunch of numbers from 0 - 64:");
+        
+        System.out.print(randomizer.nextInt(65) + ",");
+        System.out.print(randomizer.nextInt(65) + ",");
+        System.out.print(randomizer.nextInt(65) + ",");
+        System.out.print(randomizer.nextInt(65) + ",");
+        System.out.print(randomizer.nextInt(65) +",");
+        System.out.println(randomizer.nextInt(65));
+        
+        
+        
+        
+    }
+    
+}
