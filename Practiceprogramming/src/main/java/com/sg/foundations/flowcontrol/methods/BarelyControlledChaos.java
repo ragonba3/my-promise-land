74836da4357f85857c50e232ae0f17b072fee263
Rene <ren_gon@rogers.com>
2021-07-05 19:51:57 -0400

    starting arrays

--- Practiceprogramming/src/main/java/com/sg/foundations/flowcontrol/methods/BarelyControlledChaos.java
+++ Practiceprogramming/src/main/java/com/sg/foundations/flowcontrol/methods/BarelyControlledChaos.java
@@ -7,10 +7,28 @@ package com.sg.foundations.flowcontrol.methods;
 
 /**
  *
- * @author xolot
+ * @author ragonba3
  */
 public class BarelyControlledChaos {
+    public static void main(String[] args) {
+        
+        
+        String color = "magenta";
+        String animal = "Mammoth";
+        String colorAgain = "Periwinkle";
+        String weight = "80";
+        String distance = "12";
+        String number = "4593";
+        String time = "3";
     
-    
-    
-}
+             
+        System.out.println("Once,when I was very small..." );
+        
+        System.out.println("I was chased by " + color + "," + weight + " lb" + "   miniature  " + animal +  " for over  " + distance + " miles!!");
+        
+        System.out.println("I had to hide in a field of over  " + number + "  " + colorAgain + " poppies for nearly  "+ time + " hours until it left me alone!");
+        
+        System.out.println("\n It was Quite the experience" + "let me tell you!");
+        
+    }
+    }
