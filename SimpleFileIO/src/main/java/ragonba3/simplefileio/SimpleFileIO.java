b427fc3c302a2351e7deb07842e4ae604a69be73
Rene <ren_gon@rogers.com>
2021-08-07 21:49:19 -0400

    IO file

---
+++ SimpleFileIO/src/main/java/ragonba3/simplefileio/SimpleFileIO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ragonba3.simplefileio;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ragonba3
+ */
+public class SimpleFileIO {
+    
+    public static void main(String[] args) throws Exception {
+        PrintWriter out = new PrintWriter(new FileWriter("outFile.txt"));
+        
+        out.println("this a line in my file...");
+        out.println("this is a line in my file..");
+        out.println("this a third line in my file");
+        out.flush();
+        out.close();
+        
+        Scanner sc = new Scanner( new BufferedReader(new FileReader("OutFile.txt ")));
+        
+        while (sc.hasNext()){
+            String currentLine =sc.nextLine();
+            System.out.println(currentLine);
+        }
+    
+        System.out.println("That's all content in the file.");
+    }
+    
+    
+}
---
+++ StateCapitals/src/main/java/StateCapitals.java
@@ -0,0 +1,99 @@
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ragonba3
+ */
+public class StateCapitals {
+    
+    public static void main(String[] args) {
+    
+    Map<String, String> stateCapitals = new HashMap<>();
+    
+   
+			stateCapitals.put("Alabama", "Montgomery");
+                        
+                        stateCapitals.put("Alaska", "Juneau");
+                        stateCapitals.put("Arizona", "Phoenix");
+                        stateCapitals.put("Arkansas", "Little Rock");
+                        stateCapitals.put("California", "Sacramento");
+                        stateCapitals.put("Colorado", "Denver"); 
+                        stateCapitals.put("Connecticut", "Hartford"); 
+			stateCapitals.put("Delaware", "Dover");
+                        stateCapitals.put("Florida", "Tallahassee"); 
+			stateCapitals.put("Georgia", "Atlanta");
+                        stateCapitals.put("Hawaii", "Honolulu"); 
+                        stateCapitals.put("Idaho", "Boise");
+			stateCapitals.put("Illinois", "Springfield"); 
+                        stateCapitals.put("Indiana", "Indianapolis");
+			stateCapitals.put("Iowa Des", "Moines"); 
+                        stateCapitals.put("Kansas", "Topeka"); 
+                        stateCapitals.put("Kentucky","Frankfort"); 
+			stateCapitals.put("Louisiana", "Baton Rouge"); 
+                        stateCapitals.put("Maine", "Augusta"); 
+			stateCapitals.put("Maryland", "Annapolis"); 
+                        stateCapitals.put("Massachusetts", "Boston"); 
+			stateCapitals.put("Michigan", "Lansing"); 
+                        stateCapitals.put("Minnesota", "Saint Paul"); 
+			stateCapitals.put("Mississippi", "Jackson"); 
+                        stateCapitals.put("Missouri", "Jefferson City"); 
+			stateCapitals.put("Montana", "Helena"); 
+                        stateCapitals.put("Nebraska", "Lincoln"); 
+			stateCapitals.put("Nevada", "Carson City"); 
+                        stateCapitals.put("New Hampshire", "Concord"); 
+			stateCapitals.put("New Jersey", "Trenton"); 
+                        stateCapitals.put("New Mexico", "Santa Fe"); 
+			stateCapitals.put("New York", "Albany"); 
+                        stateCapitals.put("North Carolina", "Raleigh"); 
+			stateCapitals.put("North Dakota", "Bismarck");
+                        stateCapitals.put("Ohio", "Columbus");
+			stateCapitals.put("Oklahoma", "Oklahoma City");
+                        stateCapitals.put("Oregon", "Salem"); 
+			stateCapitals.put("Pennsylvania", "Harrisburg"); 
+                        stateCapitals.put("Rhode Island", "Providence"); 
+			stateCapitals.put("South Carolina", "Columbia"); 
+                        stateCapitals.put("South Dakota", "Pierre"); 
+			stateCapitals.put("Tennessee", "Nashville"); 
+                        stateCapitals.put("Texas", "Austin"); 
+			stateCapitals.put("Utah", "Salt Lake City"); 
+                        stateCapitals.put("Vermont", "Montpelier"); 
+			stateCapitals.put("Virginia", "Richmond"); 
+                        stateCapitals.put("Washington", "Olympia"); 
+			stateCapitals.put("West Virginia", "Charleston");
+                        stateCapitals.put("Wisconsin", "Madison"); 
+			stateCapitals.put("Wyoming", "Cheyenne");
+                        
+                        
+                        
+                        Set<String> keys = stateCapitals.keySet();
+                        for (String k : keys){
+                            System.out.println("States and Capitals Pairs: "+k+", "+stateCapitals.get(k));}
+                        
+                        Set <String> keys = stateCapitals.keySet()
+                        for (String k : keys){        
+                        System.out.println("State: "+k);}
+                       
+                        
+                        Collection<String>stateCapitalValue = stateCapitals.values();
+                        for(String CurrentState : stateCapitalValue){
+                            System.out.println("Capitals "+CurrentState); 
+                        
+                        }
+                        //System.out.println("State and Capitals "+ stateCapitals.get(keys) );
+                        
+    }
+    
+    
+    }
+    
+
